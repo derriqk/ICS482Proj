4fb09c73a5b51e2c560a753572084382a874bffe
Derrick Quiamas <quiamasderrick@gmail.com>
2026-05-03 15:44:28 -1000

    saving and editing

--- Plants/Assets/Scripts/background.cs
+++ Plants/Assets/Scripts/background.cs
@@ -4,11 +4,6 @@ public class background : MonoBehaviour
 {
     public GameObject glass;
     public GameObject dirt;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
